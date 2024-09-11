7e78ac8e15c2172df2721267402b2e0570b62996
guest400123064 <lgfz1003@icloud.com>
2024-09-10 22:37:46 -0400

    dual operator signatures

--- src/ezgatr/primitives/dual.py
+++ src/ezgatr/primitives/dual.py
@@ -2,12 +2,29 @@ import functools
 
 import torch
 
+from ezgatr.primitives.bilinear import geometric_product
+
 
 def dual(x: torch.Tensor) -> torch.Tensor:
     pass
 
 
 def equi_join(
-    x: torch.Tensor, y: torch.Tensor, r: torch.Tensor
+    x: torch.Tensor, y: torch.Tensor, reference: torch.Tensor
 ) -> torch.Tensor:
-    pass
+    """Compute the equivariant join of two multi-vectors given the reference.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Multi-vectors with shape (..., 16).
+    y : torch.Tensor
+        Multi-vectors with shape (..., 16).
+    reference : torch.Tensor
+        Multi-vectors with shape (..., 16).
+
+    Returns
+    -------
+    torch.Tensor
+        Equivariant join result multi-vectors with shape (..., 16).
+    """
